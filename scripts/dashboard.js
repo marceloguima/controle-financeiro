67e8301f9525965965c56952fbb86e28ef0a97e5
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-07-25 23:26:15 -0400

    criada a seção de minhas transações e consertei o bug do dropdown

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -3,8 +3,7 @@ const cardSaidas = document.getElementById("total-despesas");
 const cardSaldo = document.getElementById("saldo");
 
 const valorInserido = document.getElementById("valor");
-const tipoDaTransacao = document.getElementById("forma-transacao");
-const data = document.getElementById("data");
+
 const formulario = document.querySelector("form");
 
 formulario.addEventListener("submit", (evento) => {
@@ -14,58 +13,56 @@ formulario.addEventListener("submit", (evento) => {
 
 // ------------------------------------------------------------------
 const menuDropdownBotoes = document.querySelector(".drop-menu-botoes");
-// const MenuSelectFilter = document.querySelector(".container-campo-filter");
 const iconeAbreMenu = document.querySelector(".icone-seta");
 const overlay = document.querySelector(".overlay-dropdown");
 
-const criaOverlay = ()=>{
-    overlay.classList.toggle("active");
-}
+// --------------------------------------------------------------------
 
-const abreMenu = (menu) => {
-  
-    menu.classList.toggle("show");
-    if (menu.classList.contains("show")) {
-        iconeAbreMenu.classList.remove("fa-chevron-down");
-        
-        iconeAbreMenu.classList.add("fa-chevron-up");
-    } else {
-        iconeAbreMenu.classList.remove("fa-chevron-up");
-        iconeAbreMenu.classList.add("fa-chevron-down");
+const btnFiltraData = document.getElementById("btn-filtra-por-data");
+const btnFiltraCategoria = document.getElementById("btn-filtra-por-categoria");
+const containerParaFiltrar = document.querySelector(".drop-menu-campo-filter");
+
+const verTransacoes = () => {
+    menuDropdownBotoes.classList.toggle("show");
+    if (overlay.classList.contains("active")) {
         overlay.classList.remove("active");
+    } else {
+        overlay.classList.add("active");
     }
 };
-// --------------------------------------------------------------------
-const fechaMenu = (menu) => {
-    menu.classList.remove("show");
-    iconeAbreMenu.classList.remove("fa-chevron-up");
-    iconeAbreMenu.classList.add("fa-chevron-down");
+
+const fechaMenu = () => {
+    menuDropdownBotoes.classList.remove("show");
     overlay.classList.remove("active");
 };
-// --------------------------------------------------------------------
 
-const porCategoria = `  <div class="campo-label">
-                                <label for="campo-filter">Categoria</label>
-                        <select name="" id="categoria">
-                            <option value="salario">Meu Salário</option>
-                            <option value="freelancer">Freelancer</option>
-                            <option value="renda-extra">Renda Extra</option>
-                            <option value="outros">Outros</option>
-                        </select>
-                            </div>
-                            <button class="botao-azul filter">Buscar</button>
-                        </div>`;
+// evento para filtrar por data
+btnFiltraData.addEventListener("click", () => {
+    filtraPorData();
+});
 
+function filtraPorData() {
+    containerParaFiltrar.innerHTML = ` 
+    <div id="container-campo-filter">
+    <div class="campo-label">
+    <label>Selecione a data da transação</label
+    ><input type="date" />
+    </div>
+    <button class="botao-azul filter">Buscar</button> 
+    </div>
+    <button  onclick="fechaCampoFiltro()"
+    class="btn-opcoes fechar">
+    Fechar
+    </button>
+    `;
+    containerParaFiltrar.classList.add("show");
+    fechaMenu();
+    overlay.classList.add("active");
+}
 
-
-
-const btnCategoria = document.getElementById("btn-categoria");
-const containerParaFiltrar = document.getElementById("container-campo-filter");
-
-const filtraPorData = () => {
-    abreMenu(containerParaFiltrar)
-    containerParaFiltrar.style.display = "flex";
-    containerParaFiltrar.innerHTML =  `<div class="campo-label"><label>Selecione a data da transação</label><input type="date"></div>`;
+const fechaCampoFiltro = () => {
+    containerParaFiltrar.classList.remove("show");
+    overlay.classList.remove("active");
 };
 
 const limpaCampo = () => {
@@ -97,6 +94,7 @@ const mostraResumo = () => {
         totaldespesas += valorDigitado;
     }
     atualizaResumo();
+    criaTransacao();
     limpaCampo();
 };
 
@@ -123,34 +121,103 @@ function trocaOpcoesSelect(tipo) {
     categoria.innerHTML = "";
 
     if (tipo === "entrada") {
-        categoria.innerHTML = ` <option value="salario">Meu Salário</option>
-                                <option value="freelancer">Freelancer</option>
-                                <option value="renda-extra">Renda Extra</option>
-                                <option value="outros">Outros</option>`;
+        categoria.innerHTML = ` <option value="Salário">Meu Salário</option>
+                                <option value="Freelancer">Freelancer</option>
+                                <option value="Renda-Extra">Renda Extra</option>
+                                <option value="Outros">Outros</option>`;
 
-        formaDeTransacao.innerHTML = ` <option value="pix">Pix</option>
-                                <option value="dinheiro">Dinheiro</option>
-                                <option value="outros">Outros</option>`;
+        formaDeTransacao.innerHTML = ` <option value="Pix">Pix</option>
+                                <option value="Dinheiro">Dinheiro</option>
+                                <option value="Outros">Outros</option>`;
 
         labelFormaDeTransacao.textContent = "Forma de Recebimento";
     } else {
         categoria.innerHTML = `
-                                    <option value="moradia">Moradia</option>
-                                    <option value="mercado">Mercado</option>
-                                    <option value="saude">Saúde</option>
-                                    <option value="educacao">Educação</option>
-                                    <option value="transporte">Transporte</option>
-                                    <option value="lazer">Lazer</option>
-                                    <option value="outros">Outros</option>
+                                    <option value="Moradia">Moradia</option>
+                                    <option value="Mercado">Mercado</option>
+                                    <option value="Saude">Saúde</option>
+                                    <option value="Educacao">Educação</option>
+                                    <option value="Transporte">Transporte</option>
+                                    <option value="Lazer">Lazer</option>
+                                    <option value="Outros">Outros</option>
                                `;
 
-        formaDeTransacao.innerHTML = ` <option value="pix">Pix</option>
-                                <option value="dinheiro">Dinheiro</option>
-                                <option value="debito">Débito</option>
+        formaDeTransacao.innerHTML = ` <option value="Pix">Pix</option>
+                                <option value="Dinheiro">Dinheiro</option>
+                                <option value="Debito">Débito</option>
                                 <option value="Credito">Crédito</option>
-                                  <option value="boleto">Boleto</option>
+                                  <option value="Boleto">Boleto</option>
                               
                                 `;
         labelFormaDeTransacao.textContent = "Forma de Pagamento";
+    }
+}
+
+// mostrar transações------------------
+const formaDaTransacao = document.getElementById("forma-transacao");
+const data = document.getElementById("data");
+
+const transacao = document.querySelector(".container-transacao");
+
+const criaTransacao = () => {
+    const valorDigitado = parseFloat(valorInserido.value.trim());
+    const formaSelecionda = formaDaTransacao.value;
+    const diaDaTransacao = data.value;
+    const categoriaDaTransacao = categoria.value;
+
+    const valorRadio = document.querySelector(
+        'input[name="transacao"]:checked'
+    ).value;
+
+    let icon = "";
+
+    if (valorRadio === "entrada") {
+        icon = `<svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                class="bi bi-arrow-up-circle-fill arrow-up"
+                viewBox="0 0 16 16"
+            >
+                <path
+                    d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"
+                />
+            </svg>`;
+    } else {
+        icon = ` <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    class="bi bi-arrow-down-circle-fill arrow-down"
+                    viewBox="0 0 16 16"
+                >
+                    <path
+                        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"
+                    />
+                </svg>`;
+    }
+
+    transacao.innerHTML += ` <div class="transacao">
+                    <div class="div-icon-categoria">
+                         ${icon}
+                        <h4>${categoriaDaTransacao}</h4>
+                    </div>
+                    <p class="forma-transacao">${formaSelecionda}</p>
+                    <p class="data-transacao">${diaDaTransacao}</p>
+                    <p class="valor-transacao"> R$ ${valorDigitado}</p>
+                </div>
+
+            </section>
+        </div>
+`;
+};
+
+const mensagemSeTransacao = document.querySelector(".mensagem-campo-transacao")
+
+const mostraMensagemSemTransacao = ()=>{
+    if(!transacao.innerText ){
+        mensagemSeTransacao.textContent = "Nenhuma transação encontrada"
     }
 }
